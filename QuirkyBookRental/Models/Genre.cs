633fd030ae6623f6a170bc8ca3130dac5d64e906
CCOLOMER <CCOLOMER@PCDESARROLLO200>
2022-05-12 17:14:20 +0200

    prueba

---
+++ QuirkyBookRental/Models/Genre.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace QuirkyBookRental.Models
+{
+    public class Genre
+    {
+        [Required]
+        public int id { get; set; }
+        [Required]
+        public int Name { get; set; }
+    }
+}
