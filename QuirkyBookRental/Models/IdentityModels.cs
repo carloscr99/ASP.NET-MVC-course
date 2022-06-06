4086a90df9a855440841b7d844f3d41309fdbd99
carloscr99 <carloscolomer99@gmail.com>
2022-06-06 17:26:13 +0200

    Added new fields to an existing database (user table)

--- QuirkyBookRental/Models/IdentityModels.cs
+++ QuirkyBookRental/Models/IdentityModels.cs
@@ -1,4 +1,5 @@
-﻿using System.Data.Entity;
+﻿using System;
+using System.Data.Entity;
 using System.Security.Claims;
 using System.Threading.Tasks;
 using Microsoft.AspNet.Identity;
@@ -9,6 +10,16 @@ namespace QuirkyBookRental.Models
     // Para agregar datos de perfil del usuario, agregue más propiedades a su clase ApplicationUser. Visite https://go.microsoft.com/fwlink/?LinkID=317594 para obtener más información.
     public class ApplicationUser : IdentityUser
     {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Phone { get; set; }
+        public DateTime BirthDate { get; set; }
+        public bool Disable { get; set; }
+        public int MembershipTypeId { get; set; }
+        
+        
+        
+
         public async Task<ClaimsIdentity> GenerateUserIdentityAsync(UserManager<ApplicationUser> manager)
         {
             // Tenga en cuenta que el valor de authenticationType debe coincidir con el definido en CookieAuthenticationOptions.AuthenticationType
