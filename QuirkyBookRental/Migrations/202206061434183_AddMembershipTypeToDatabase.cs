fbaded3d07fc5e9698da2f0618f0c2375232b120
carloscr99 <carloscolomer99@gmail.com>
2022-06-06 17:17:32 +0200

    CRUD membership types

---
+++ QuirkyBookRental/Migrations/202206061434183_AddMembershipTypeToDatabase.cs
@@ -0,0 +1,20 @@
+﻿namespace QuirkyBookRental.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class AddMembershipTypeToDatabase : DbMigration
+    {
+        public override void Up()
+        {
+            AddColumn("dbo.MembershipTypes", "SignUpFee", c => c.Byte(nullable: false));
+            DropColumn("dbo.MembershipTypes", "SingUpFee");
+        }
+        
+        public override void Down()
+        {
+            AddColumn("dbo.MembershipTypes", "SingUpFee", c => c.Byte(nullable: false));
+            DropColumn("dbo.MembershipTypes", "SignUpFee");
+        }
+    }
+}
